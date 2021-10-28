39085b2c325972f38a0b203a3a99a3a95c631db3
Natalia Fedorova <nataliafedorova1989@gmail.com>
2021-10-28 14:17:05 +0200

    login w-i-p

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,27 +3,42 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 // Project files
-import  InputField  from "../components/InputField";
+import InputField  from "../components/InputField";
 import fields from "../data/fields-login.json";
+import { signIn } from "scripts/authentification";
+import { getDocument } from "scripts/fireStore";
 
 export default function Login() {
     // Local state
-    const [user, setUser] = useState( { email: "", password: "" })
+    const [form, setForm] = useState({});
+    const [errorMassage, setErrorMessage] = useState("");
 
     // Methods
     function onChange(key, value) {
         const field = { [key]: value };
-        setUser({...user, ...field});
+        setForm({...form, ...field});
     }
 
-    function onSubmit(event) {
+    async function onSubmit(event) {
         event.preventDefault();
-        alert("On Submit...")
+        setErrorMessage("");
+        const account = await signIn(form.email, form.password);
+
+        account.isLogged ? onSuccess(account.payload) : onFailure(account.payload);
+    }
+
+    async function onSuccess(uid){
+        const user = await getDocument("users", uid)
+        console.log("Login.jsx onSuccess() user", user);
+    }
+
+    function onFailure(message){
+        setErrorMessage(message);
     }
 
     // Components
     const InputFields = fields.map((item) => (
-        <InputField key={item.key} options={item} state={user[item.key]} onChange={onChange} />
+        <InputField key={item.key} options={item} state={form[item.key]} onChange={onChange} />
     ));
 
     return (
@@ -31,6 +46,7 @@ export default function Login() {
             <h1>Login</h1>
             <form onSubmit={onSubmit}>
                 {InputFields}
+                <p>{errorMassage}</p>
                 <div>
                     <button>Login</button>
                 </div>
--- src/scripts/authentification.js
+++ src/scripts/authentification.js
@@ -1,19 +1,40 @@
 // NPM Packages
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 // Project files
 import { authInstance } from "./firebase";
 
 export async function createAccount(email, password) {
-  const account = { isCreated: false, payload: null };
+  const account = { isCreated: false, payload: "" };
   try {
     const userCredential = await createUserWithEmailAndPassword(
       authInstance,
       email,
       password
     );
-    account.isCreated = true;
     account.payload = userCredential.user.uid;
+    account.isCreated = true;
+  } catch (error) {
+    console.error("authentification.js error", error);
+    account.payload = error.code;
+  }
+
+  return account;
+}
+
+export async function signIn(email, password) {
+  const account = { isLogged: false, payload: "" };
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      authInstance,
+      email,
+      password
+    );
+    account.payload = userCredential.user.uid;
+    account.isLogged = true;
   } catch (error) {
     console.error("authentification.js error", error);
     account.payload = error.code;
